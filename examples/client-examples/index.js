0c1f42320c06eb79e4c52011fce3944e9f7a8751
Peter Heesterman <peterheesterman@Administrators-MacBook-Pro-2.local>
2017-09-01 11:24:47 +1200

    Add the local ip

--- examples/client-examples/index.js
+++ examples/client-examples/index.js
@@ -7,7 +7,8 @@ let data = {
 }
 
 request.post(
-    'http://localhost:3000/play',
+    'http://192.168.88.54:3000/play',
+    //'http://localhost:3000/play',
     { json: data },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
