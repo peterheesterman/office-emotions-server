a5974224350dbd2832ad3975e0ffef5c1423a90a
Peter Heesterman <peterheesterman@Administrators-MacBook-Pro-2.local>
2017-09-01 11:32:50 +1200

    Make the data name correct

--- examples/client-examples/index.js
+++ examples/client-examples/index.js
@@ -4,13 +4,13 @@ var request = require('request');
 var emo = 'angry';
 
 let data = {
-  text: emo,
+  name: emo,
 }
 
 request.post(
-    'http://192.168.88.54:3000/play',
+    //'http://192.168.88.54:3000/play',
     //'http://192.168.88.54:3000/slack',
-    //'http://localhost:3000/play',
+    'http://localhost:3000/play',
     //'http://localhost:3000/slack',
     { json: data },
     function (error, response, body) {
